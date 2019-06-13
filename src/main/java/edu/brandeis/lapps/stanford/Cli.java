5bc52de359c4d117bbf3856ea35a42faf1328af9
Keigh Rim <keigh.rim@gmail.com>
2019-06-12 21:32:02 -0400

    CLI helpers are moved to scaffolding, code clean-up

--- src/main/java/edu/brandeis/lapps/stanford/Cli.java
+++ src/main/java/edu/brandeis/lapps/stanford/Cli.java
@@ -1,16 +1,14 @@
 package edu.brandeis.lapps.stanford;
 
+import edu.brandeis.lapps.CliUtil;
 import edu.brandeis.lapps.stanford.corenlp.*;
 
-import java.io.*;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.time.Instant;
-import java.time.ZoneOffset;
-import java.time.format.DateTimeFormatter;
-import java.util.Objects;
 
 /**
  * A Class to provide command line interface to run a specific tool in this
@@ -24,49 +22,22 @@ public class Cli {
 
     private static Class<? extends AbstractStanfordCoreNLPWebService> toolClass;
 
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
+    public static void main(String[] args) throws IOException, ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
         if (args.length < 1) {
             help();
         } else {
             toolClass = pickToolClass(args[0]);
+            AbstractStanfordCoreNLPWebService tool = toolClass.getConstructor().newInstance();
             if (args.length == 1 || args[1].equals("-")) {
-                System.out.println(processInputStream(System.in));
+                System.out.println(CliUtil.processInputStream(tool, System.in));
             } else {
                 Path inputPath = Paths.get(args[1]);
                 if (Files.exists(inputPath) && Files.isDirectory(inputPath)) {
-                    processDirectory(inputPath, args[0]);
+                    CliUtil.processDirectory(tool, inputPath, args[0]);
                 } else {
-                    System.out.println(processInputStream(new FileInputStream(inputPath.toFile())));
+                    System.out.println(CliUtil.processInputStream(tool, new FileInputStream(inputPath.toFile())));
                 }
             }
-        }
-    }
-
-    private static void processDirectory(Path inDir, String annName) throws IOException {
-        Path outDir = Paths.get(inDir.toString(),
-                annName + DateTimeFormatter.ofPattern("-yyyyMMdd'T'HHmmssX").withZone(ZoneOffset.UTC).format(Instant.now()));
-        Files.createDirectory(outDir);
-        for (File lifFile : Objects.requireNonNull(inDir.toFile().listFiles((dir, name) -> name.charAt(0) != '.' && name.endsWith(".lif")))) {
-            Path outFile = outDir.resolve(Paths.get(lifFile.getName()).getFileName());
-            java.nio.file.Files.write(outFile, processInputStream(new FileInputStream(lifFile)).getBytes());
-        }
-    }
-
-    private static String processInputStream(InputStream is) throws IOException {
-        try {
-            AbstractStanfordCoreNLPWebService tool = toolClass.getConstructor().newInstance();
-            ByteArrayOutputStream toString = new ByteArrayOutputStream();
-            byte[] buffer = new byte[10240];
-            int length;
-            while ((length = is.read(buffer)) != -1) {
-                toString.write(buffer, 0, length);
-            }
-            String inString =  toString.toString("UTF-8");
-            return tool.execute(inString);
-        } catch (InstantiationException | IllegalAccessException | InvocationTargetException
-                | NoSuchMethodException ignored) {
-            // will not happen
-            return "";
         }
     }
 
